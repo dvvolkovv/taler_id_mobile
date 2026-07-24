05c990673c7efbd51ecb8da937ae9a991d9da341
Dmitry Volkov <dvvolkovv@gmail.com>
2026-07-24 11:55:21 +0300

    feat(mail): inbox, detail, compose screens

---
+++ lib/features/mail/presentation/screens/mail_app_passwords_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MailAppPasswordsScreen extends StatelessWidget {
+  const MailAppPasswordsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('TODO: App Passwords')));
+  }
+}
